54ae0266dd89b7b80eb866e8743f9ebaca3e7c53
Marielle <marielle.ibias@example.com>
2022-09-13 13:21:38 +0200

    crud implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
-
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import { db } from "./firebase-config";
+import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from "firebase/firestore";
 
 function App() {
+  const [newName, setNewName] = useState("");
+  const [newAge, setNewAge] = useState(0);
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+  const createUser = async () => {
+    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  };
+
+  const updateUser = async (id, age) => {
+    const userDoc = doc(db, "users", id);
+    const newFields = { age: age + 1 };
+    await updateDoc(userDoc, newFields);
+  };
+
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, "users", id);
+    await deleteDoc(userDoc);
+  }
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getUsers();
+  }, [usersCollectionRef]);
+
   return (
     <div className="App">
-
+      <h1>Hello</h1>
+      <input
+        placeholder="Name"
+        onChange={(e) => {
+          setNewName(e.target.value);
+        }}
+      />
+      <input
+        type="number"
+        placeholder="Age"
+        onChange={(e) => {
+          setNewAge(e.target.value);
+        }}
+      />
+      <button onClick={createUser}>Create User</button>
+      {users.map((users) => {
+        return (
+          <div>
+            <h1>Name: {users.name}</h1>
+            <h1>Age: {users.age}</h1>
+            <button
+              onClick={() => {
+                updateUser(users.id, users.age);
+              }}
+            >
+              {" "}
+              Increase Age
+            </button>
+            <button
+              onClick={() => {
+                deleteUser(users.id);
+              }}
+            >
+              Delete User
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 }
